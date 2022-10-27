b9380615d0dc8bc4c924f52efb7aa3af218c5968
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-10-27 15:17:23 +0530

    commit

--- Frontend/src/app/components/register/register.component.ts
+++ Frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ElementRef} from '@angular/core';
 import { ValidateService } from 'src/app/services/validate.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
@@ -9,7 +10,11 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  
+
+  description = document.getElementsByClassName('description');
+  testhtml:string = '';
+  alertStyle:string = '';
+  pwdType:boolean = false;
   backgroundUrl=" https://source.unsplash.com/YkA7CLrMg8g"
   
   firstName:string = '';
@@ -21,26 +26,69 @@ export class RegisterComponent implements OnInit {
   password:string = '';
   repeatPassword:string = '';
   
+  pwdToggle():void{
+    this.pwdType = !this.pwdType;
+  }
+
   constructor(
     private toastr: ToastrService,
     private validateService: ValidateService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private elementRef:ElementRef
     ) { }
 
   ngOnInit(): void {
   }
+  
+
+
+// Checking Password Strength
+checkstrength(){
+  const user = {
+    password: this.password.trim()
+  }
+
+// Checking Password Minimum Character Criteria
+  if(!this.validateService.isPwdMatchCriteria(user.password)){
+    this.testhtml = "<p>Password Did Not satisfy Minimum Character Criteria</p>";
+    this.alertStyle = 'weakPwd';
+  }
+
+
+if(this.validateService.ispstrong(user.password)){
+  // https://stackoverflow.com/questions/41979458/how-to-get-angular2-innerhtml-to-work
+  this.testhtml = "<p>Strong Password</p>";
+  this.alertStyle = 'strongPwd';
+  return true;
+  // 123456789Aa@
+
+}
+if(this.validateService.ispmedium(user.password)){
+  this.testhtml = "<p>Medium Password</p>";
+  this.alertStyle = 'mediumPwd';
+  return true;
+  // 123456Aa@
+
+}
+if(this.validateService.isPassword(user.password)){
+  this.testhtml = "<p>Weak Password</p>";
+  this.alertStyle = 'weakPwd';
+  // 123456Aa
+  return true;
+}
+};
 
   onRegisterSubmit(){
     const user = {
-      firstName: this.firstName,
-      lastName: this.lastName,
-      gender: this.gender,
-      phone: this.phone,
-      email: this.email,
-      username: this.email,
-      password: this.password,
-      repeatPassword: this.repeatPassword
+      firstName: this.firstName.trim(),
+      lastName: this.lastName.trim(),
+      gender: this.gender.trim(),
+      phone: this.phone.trim(),
+      email: this.email.trim(),
+      username: this.email.trim(),
+      password: this.password.trim(),
+      repeatPassword: this.repeatPassword.trim()
     }
 
      // required fields
@@ -48,11 +96,15 @@ export class RegisterComponent implements OnInit {
   this.toastr.error('Please Fill in all fields');
   return false;
 }
+
 // validate email
 if(!this.validateService.validateEmail(user.email)){
   this.toastr.error('Please use Valid Email');
     return false;
 }
+
+
+
 // retype password
 if(!this.validateService.repeatPassword(user)){
   this.toastr.error('Passwords Did Not Match');
--- Frontend/src/app/services/validate.service.ts
+++ Frontend/src/app/services/validate.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidateService {
 
-  constructor() { }
+  constructor(private toastr:ToastrService) { }
 
   validateRegister(user:any){
     if(user.firstName == "" || user.phone == "" ||user.email == "" || user.password == "")
@@ -15,6 +16,7 @@ export class ValidateService {
       }
   } 
 
+  
   repeatPassword(user:any){
     if(user.password === user.repeatPassword)
       return true;
@@ -44,5 +46,21 @@ export class ValidateService {
     return regex.test(email);
   }
   
+  isPassword(password:any){
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+  }
+  ispstrong(password:any){
+    return /^((?=.*[a-z])(?=.*[A-Z])(?=.*[10-15])(?=.*[^A-Za-z0-9])(?=.{11,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{11,}))$/.test(password);
+  
+  }
+  ispmedium(password:any){
+    return /^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,10}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,10}))$/.test(password);
+  }
+
+  isPwdMatchCriteria(password:any){
+    if((password.length)<8){
+      return false;
+    }
+  }
 
 }
